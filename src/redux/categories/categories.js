c3720a0cdd7e68880704bade40bb111e0404c677
Emmanuel Paul <emmapaul247@gmail.com>
2022-06-15 23:21:28 +0100

    Update categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,15 +1,15 @@
-// const ACTIVE = 'redux/categories/ACTIVE';
-// const initialState = null;
-// const createActionForCategory = () => ({ type: ACTIVE });
+const ACTIVE = 'redux/categories/ACTIVE';
+const initialState = null;
+const createActionForCategory = () => ({ type: ACTIVE });
 
-// const reducerForCategory = (state = initialState, action) => {
-//   switch (action.type) {
-//     case ACTIVE:
-//       return 'Under construction';
-//     default:
-//       return state;
-//   }
-// };
+const reducerForCategory = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTIVE:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
 
-// export default reducerForCategory;
-// export { createActionForCategory };
+export default reducerForCategory;
+export { createActionForCategory };
